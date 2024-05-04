7564f38c70c864d02d83e48b50275a6dedcfca0e
leht377 <migithub2019@gmail.com>
2024-05-03 20:19:24 -0500

    obtenerReservaPorId

--- src/domain/datasources/reserva.datasource.ts
+++ src/domain/datasources/reserva.datasource.ts
@@ -1,8 +1,11 @@
-import { ActualizarReservaDto } from '../dtos'
+import { ActualizarReservaDto, ObtenerReservaPorIdDto } from '../dtos'
 import { SolicitarReservaDto } from '../dtos/reserva/solicitar-reserva.dto'
 import { ReservaEntity } from '../entities'
 
 export abstract class ReservaDatasource {
   abstract registrarReserva(solicitarReservaDto: SolicitarReservaDto): Promise<ReservaEntity>
   abstract actualizarReserva(actualizarReservaDto: ActualizarReservaDto): Promise<ReservaEntity>
+  abstract obtenerReservaPorId(
+    obtenerReservaPorIdDto: ObtenerReservaPorIdDto
+  ): Promise<ReservaEntity | null>
 }
